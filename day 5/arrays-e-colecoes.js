577134228e162fa00e31427827a7b5f0f835d542
alanisboeing@gmail.com <alanisboeing@gmail.com>
2023-04-24 10:23:06 -0300

    fazendo alterações aplicando novos conhecimentos, como funções anonimas, for each, e pequenas modificações no html

--- day 5/arrays-e-colecoes.js
+++ day 5/arrays-e-colecoes.js
@@ -1,15 +1,35 @@
 
-var botao = document.querySelector('.botao');
-var botao2 = document.querySelector('.botao2');
+var botaoAdicionaNaLista = document.querySelector("[data-botao]");
+var botaoOtimizaLista = document.querySelector("[data-botao-otimiza]");
 
-botao.onclick = function () {
-    var dadoItem = document.querySelector('.item').value;
-    var dadoCategoria = document.querySelector('.categoria').value;
+// botaoAdicionaNaLista.onclick = function () {
+//     var dadoItem = document.querySelector('.item').value;
+//     var dadoCategoria = document.querySelector('.categoria').value;
 
-    document.querySelector('.item').value = '';
-    document.querySelector('.categoria').value = '';
+//     document.querySelector('.item').value = '';
+//     document.querySelector('.categoria').value = '';
 
-    //tabela
+//     //tabela
+//     var table = document.getElementById("lista");
+//     var row1 = table.insertRow(1);
+//     var item = row1.insertCell(0);
+//     var categoria = row1.insertCell(1);
+
+//     item.innerHTML = dadoItem;
+//     categoria.innerHTML = dadoCategoria;
+    
+// }
+
+botaoAdicionaNaLista.addEventListener( "click" , () => {
+    var dadoItem = document.querySelector(".item").value;
+    var dadoCategoria = document.querySelector(".categoria").value;
+
+   // document.querySelector("[data-item]").value = '';
+   // document.querySelector("[data-categoria]").value = '';
+   document.querySelector('.item').value = '';
+   document.querySelector('.categoria').value = '';
+
+    // tabela
     var table = document.getElementById("lista");
     var row1 = table.insertRow(1);
     var item = row1.insertCell(0);
@@ -17,12 +37,12 @@ botao.onclick = function () {
 
     item.innerHTML = dadoItem;
     categoria.innerHTML = dadoCategoria;
-    
-}
+})
 
-botao2.onclick = function () {
+botaoOtimizaLista.onclick = function () {
     var table = document.getElementById("lista");
-    var totalItens = table.rows.length;
+    //var rows = table.rows;
+    var rows = document.querySelectorAll("#lista tr");;
     var itensPadaria = [];
     var itensHortifruti = [];
     var itensFrios = [];
@@ -30,33 +50,61 @@ botao2.onclick = function () {
     var itensLimpeza = []
     var itensOutros = [];
 
-    for (contador = 1; contador < totalItens; contador++) {
-        var row = table.rows.item(contador);
+    console.log(rows);
+
+    rows.forEach( ( row )=> {
         var categoria = row.cells.item(1).innerHTML;
-        var item = row.cells.item(0).innerHTML;
+         var item = row.cells.item(0).innerHTML;
         
-        if (categoria == 'padaria'){
-            itensPadaria.push(item);
-        }        
-        if (categoria == 'hortifruti'){
-            itensHortifruti.push(item);
-        }
-        if (categoria == 'frios'){
-            itensFrios.push(item);
-        }
-        if (categoria == 'higiene'){
-            itensHigiene.push(item);
-        }
-        if (categoria == 'limpeza'){
-            itensLimpeza.push(item);
-        }
-        if (categoria == 'outros'){
-            itensOutros.push(item);
-        }
-    }
+         if (categoria == 'padaria'){
+             itensPadaria.push(item);
+         }        
+         if (categoria == 'hortifruti'){
+             itensHortifruti.push(item);
+         }
+         if (categoria == 'frios'){
+             itensFrios.push(item);
+         }
+         if (categoria == 'higiene'){
+             itensHigiene.push(item);
+         }
+         if (categoria == 'limpeza'){
+             itensLimpeza.push(item);
+         }
+         if (categoria == 'outros'){
+             itensOutros.push(item);
+         }
+     }
+
+    );
+
+    // for (contador = 1; contador < totalItens; contador++) {
+    //     var row = table.rows.item(contador);
+    //     var categoria = row.cells.item(1).innerHTML;
+    //     var item = row.cells.item(0).innerHTML;
+        
+    //     if (categoria == 'padaria'){
+    //         itensPadaria.push(item);
+    //     }        
+    //     if (categoria == 'hortifruti'){
+    //         itensHortifruti.push(item);
+    //     }
+    //     if (categoria == 'frios'){
+    //         itensFrios.push(item);
+    //     }
+    //     if (categoria == 'higiene'){
+    //         itensHigiene.push(item);
+    //     }
+    //     if (categoria == 'limpeza'){
+    //         itensLimpeza.push(item);
+    //     }
+    //     if (categoria == 'outros'){
+    //         itensOutros.push(item);
+    //     }
+    // } 
 
     var texto = "Padaria: " + itensPadaria + ". Hortifruti: " + itensHortifruti + ". Frios: " + itensFrios + ". Higiene: " + itensHigiene + ". Limpeza: " + itensLimpeza + ". Outros: " + itensOutros + "." ;
-    document.getElementById("texto").value=texto;
+    var listaOtimizada = alert(texto);
 
     
 
